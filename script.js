e26e4420fcc8a266081fa0a04503d3b054b59255
itsarchy <75260573+itsarchy@users.noreply.github.com>
2020-11-30 20:38:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var is_mobile=700
 var pc_width=75;
 var pc_menu=25;
 
-if (window.screen.availWidth<is_mobile){
+if (document.body.clientWidth<is_mobile){
     mobile=1;
 }
 else{
@@ -50,7 +50,7 @@ function showHide() {
 }
 
 function screen_test(){
-    if (window.screen.availWidth<is_mobile){
+    if (document.body.clientWidth<is_mobile){
           mobile=1;
     }
     else{
